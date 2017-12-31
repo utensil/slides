2e667be1292b363766b6f15b2bb6ead4baa25b3d
utensil <utensilcandel@gmail.com>
2017-12-31 12:17:04 +0800

    A script to install certain dependencies

---
+++ ml/scripts/install.sh
@@ -0,0 +1,10 @@
+npm install -g reveal-md
+npm install -g mathjax-node-cli
+
+# Intall graphviz
+# http://www.graphviz.org/Download_windows.php
+# http://www.graphviz.org/Download..php
+# brew install graphviz
+
+# http://ogom.github.io/draw_uml/plantuml/
+npm install -g node-plantuml
