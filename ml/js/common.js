7fe84022ba8dd39ef67060ae5e4790095dadea89
utensil <utensilcandel@gmail.com>
2017-12-31 12:15:06 +0800

    Show iframe sources when printing to pdf with Chrome

--- ml/js/common.js
+++ ml/js/common.js
@@ -32,6 +32,28 @@
 //   renderer: markdownRenderer
 // });
 
+// https://stackoverflow.com/a/14570614/200764
+var observeDOM = (function(){
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+      eventListenerSupported = window.addEventListener;
+
+  return function(obj, callback){
+      if( MutationObserver ){
+          // define a new observer
+          var obs = new MutationObserver(function(mutations, observer){
+              if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+                  callback();
+          });
+          // have the observer observe foo for changes in children
+          obs.observe( obj, { childList:true, subtree:true });
+      }
+      else if( eventListenerSupported ){
+          obj.addEventListener('DOMNodeInserted', callback, false);
+          obj.addEventListener('DOMNodeRemoved', callback, false);
+      }
+  };
+})();
+
 Reveal.addEventListener('ready', function () {
 
   head.load('./_assets/js/reveal-code-focus-modified.js', function () {
@@ -39,6 +61,36 @@ Reveal.addEventListener('ready', function () {
     console.log(window.hljs);
     RevealCodeFocus();
   });
+
+  if(window.location.search.match( /print-pdf/gi )) {
+    var timeOfLastUpdate = new Date();
+    var idleTime = 10000 /* ms */;
+
+    observeDOM(document.querySelector('.reveal'), function () {
+      // console.log('observeDOM', Date.now() - timeOfLastUpdate);
+
+      timeOfLastUpdate = Date.now();
+
+      setTimeout(function () {
+        if (Date.now() - timeOfLastUpdate > idleTime) {
+          var allBgs = document.querySelectorAll('.print-pdf .pdf-page .slide-background');
+          
+          allBgs.forEach(function (bg) {
+            var iframeBg = bg.querySelector('iframe');
+            if(iframeBg != null) {
+              var url = iframeBg.getAttribute('src');
+              iframeSource = document.createElement('div');
+              iframeSource.innerHTML = '<div class="iframe-source">Source: <a target="_blank" href="' + url + '">' 
+              + url + '</a></div>';
+              bg.appendChild(iframeSource);
+            }
+          });
+  
+          console.log('iframe source added!');
+        }
+      }, idleTime);      
+    });
+  }
 });
 
 Reveal.addEventListener('slidechanged', function (event) {
