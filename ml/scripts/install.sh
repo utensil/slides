6ed5cb2261d7c40d8e87f8413c6464addb7c6dd0
utensil <utensilcandel@gmail.com>
2018-07-02 23:03:34 +0800

    Try to improve the installation of plantuml

--- ml/scripts/install.sh
+++ ml/scripts/install.sh
@@ -1,13 +1,15 @@
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
 npm install -g reveal-md
 npm install -g mathjax-node-cli
 
 # Intall graphviz
-# http://www.graphviz.org/Download_windows.php
+# https://graphviz.gitlab.io/_pages/Download/Download_windows.html
 # http://www.graphviz.org/Download..php
 # brew install graphviz
 
 # http://ogom.github.io/draw_uml/plantuml/
 npm install -g node-plantuml
+npm link node-plantuml
 
-# Add `npm root -g` to NODE_PATH
-export NODE_PATH=$NODE_PATH:`npm root -g`
+# # Add `npm root -g` to NODE_PATH
+# export NODE_PATH=$NODE_PATH:`npm root -g`
